88bbe61e7d20aaa795cac002fd531d2d6c9e4f9a
Edgar Quezada <edgar.quezada@gmail.com>
2024-04-26 11:03:20 +0200

    slice of int definition

--- 070-for-range/main.go
+++ 070-for-range/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	// for range loop
 	// data structures - slice
-	xi := []int{42, 43, 44, 45, 46, 47}
+	xi := []int{42, 43, 44, 45, 46, 47} //slice of int with range of values
 	for i, v := range xi {
 		fmt.Println("ranging over a slice", i, v)
 	}

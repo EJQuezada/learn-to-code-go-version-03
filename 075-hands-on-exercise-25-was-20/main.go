c14dd20f512943c1f637422b31dfafa5a5114fff
Edgar Quezada <edgar.quezada@gmail.com>
2024-04-26 18:53:55 +0200

    comment out  if section to replace with switch statement

--- 075-hands-on-exercise-25-was-20/main.go
+++ 075-hands-on-exercise-25-was-20/main.go
@@ -9,16 +9,16 @@ func main() {
 	x := rand.Intn(400)
 	fmt.Printf("The value of x is %v\t", x)
 
-	/*
-		if x <= 100 {
-			fmt.Println("less than 100")
-		} else if x >= 101 && x <= 200 {
-			fmt.Println("between 101 and 200")
-		} else if x >= 201 && x <= 250 {
-			fmt.Println("between 201 and 250")
-		} else {
-			fmt.Println("this was more than 250")
-		}
+	/* comment out this section to use switch in next section
+	if x <= 100 {
+		fmt.Println("less than 100")
+	} else if x >= 101 && x <= 200 {
+		fmt.Println("between 101 and 200")
+	} else if x >= 201 && x <= 250 {
+		fmt.Println("between 201 and 250")
+	} else {
+		fmt.Println("this was more than 250")
+	}
 	*/
 
 	switch {

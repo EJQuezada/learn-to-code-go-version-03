8340c1d91a3d66abc47ec0375588205baceba667
Edgar Quezada <edgar.quezada@gmail.com>
2024-04-27 10:20:13 +0200

    formatted hands on exercise 39 to even spacing in last print operation

--- 089-hands-on-exercise-39-was-34/main.go
+++ 089-hands-on-exercise-39-was-34/main.go
@@ -7,5 +7,5 @@ func main() {
 	fmt.Printf("true && false \t\t%v\n", (true && false))
 	fmt.Printf("true || true \t\t%v\n", (true || true))
 	fmt.Printf("true || false \t\t%v\n", (true || false))
-	fmt.Printf("!true \t\t\t%v\n", !true)
+	fmt.Printf("!true \t\t\t\t%v\n", !true)
 }

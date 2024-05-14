c8e6cafa861f41d603d83d379d60d4addced0a7b
Edgar Quezada <edgar.quezada@gmail.com>
2024-05-14 10:26:06 +0200

    fix typo inwrite string It's Thursday

--- 140-writer-interface-byte-buffer/main.go
+++ 140-writer-interface-byte-buffer/main.go
@@ -33,9 +33,9 @@ func main() {
 	b.WriteString("Gophers!")
 	fmt.Println(b.String())
 	b.Reset()
-	b.WriteString("It's Thursday")
+	b.WriteString("It's Thursday ")
 	fmt.Println(b.String())
-	
+
 	b.Write([]byte("Happy Happy"))
 	fmt.Println(b.String())
 }

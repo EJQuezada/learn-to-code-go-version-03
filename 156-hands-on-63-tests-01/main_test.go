aacf98f814794eb17d855dcc49fdd40663cd09e4
Edgar Quezada <edgar.quezada@gmail.com>
2024-05-15 14:23:01 +0200

    add comment to test.go file

--- 156-hands-on-63-tests-01/main_test.go
+++ 156-hands-on-63-tests-01/main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-    total := Add(5, 5)
-    if total != 10 {
-        t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
-    }
+	total := Add(5, 5) //in terminal run to get PASS status. If incorrect will let you know FAIL
+	if total != 10 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
 }

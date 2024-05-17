5e62933ea4709c3c1117a23be7a8749f9ab630a8
Edgar Quezada <edgar.quezada@gmail.com>
2024-05-17 12:03:35 +0200

    addd comment re pointer receiver

--- 175-method-sets-part-2/main.go
+++ 175-method-sets-part-2/main.go
@@ -29,8 +29,8 @@ func main() {
 	d1.walk()
 	d1.run()
 	// youngRun(d1) // doesn't run
-	
-	d2 := &dog{"Padget"}
+
+	d2 := &dog{"Padget"} //&dog is pointer
 	d2.walk()
 	d2.run()
 	youngRun(d2)
@@ -41,11 +41,11 @@ func main() {
 
 The idea of the method set is integral to how interfaces are implemented and used in Go.
 
-The method set of a type T consists of all methods with receiver type T. 
+The method set of a type T consists of all methods with receiver type T.
 These methods can be called using variables of type T.
 
-The method set of a type *T consists of all methods with receiver *T or T 
+The method set of a type *T consists of all methods with receiver *T or T
 These methods can be called using variables of type *T.
-it can call methods of the corresponding non-pointer type as well 
+it can call methods of the corresponding non-pointer type as well
 
 */
